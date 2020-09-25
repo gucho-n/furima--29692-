be48119fbe30f57aa95bc1cf22ef79e619932791
gucho-n <i.m.your.dog.1192@gmail.com>
2020-09-25 12:25:40 +0900

    ログインまで完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-end
+  before_action :basic_auth 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #configure_permitted_parameters, if: :devise_controller?は慣習だと覚えておこう。
 
 
-class ApplicationController < ActionController::Base
-  before_action :basic_auth
-  (省略)
+
+
 
   private
 
@@ -14,4 +14,13 @@ class ApplicationController < ActionController::Base
      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:birthday,:surname,:surname_kana,:firstname,:firstname_kana])
+    #devise内でパラメーターを使うには、許可がいる。devise_parameter_sanitizerメソッドを使うことによって承認が降りる
+    #早めに承認しておきたいので、beforeアクションがいる
+    # permit(:deviseの処理名, keys: [:許可するキー])
+    # このデバイスの処理名はsign_up,sign_in,account_update
+  end
+
+  # コントローラーどーするかわからん！クリエイト？
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users 
-  # デバイスをインストールしていればresourcesいらないと思ったんだけどなぁ→多分正解
+  # デバイスをインストールしていればresourcesいらないと思ったんだけどなぁ。どこまでができおう範囲内？
+
   root to: 'items#index'
+  
   
 
 end
