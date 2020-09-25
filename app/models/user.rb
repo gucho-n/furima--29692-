842dc7d9292dfaf4beeca42b61c101b29eb0e9da
gucho-n <i.m.your.dog.1192@gmail.com>
2020-09-25 15:43:46 +0900

    パスワードの英数字混合

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  # emailとpasswordについてはデフォルトで「存在しないと保存できない」というバリデーションが用意されている
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  # あってるかわからないが英数字指定は正規表現を使う。赤字の部分を代入してパスワードに使う
+  # マッチは特定の値をチェックするだけ
+ 
+ 
 end
