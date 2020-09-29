9d11a0c79646f19934084fc5f26058c73c56ca9b
gucho-n <i.m.your.dog.1192@gmail.com>
2020-09-29 16:54:43 +0900

    自分用メモ編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,7 @@ class ItemsController < ApplicationController
   def index
     @items = Item.order('created_at DESC')
   end
-  # 原則画面遷移する前に下記のアクションは発動してないので
-  # 画面遷移する前にインスタンス変数を生成する場合、インデックスであらかじめ生成しておく必要がある
+  
 
   def new
     @item = Item.new
@@ -15,8 +14,7 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  # Viewのなかに例えば2というIDを入力して
-  # バイプラ先生やったら問題なくできた
+ 
 
   def create
     @item = Item.new(item_params)
@@ -66,3 +64,10 @@ end
 # （この時にcreateは、生成・保存に分割、インスタンス変数を生成して、保存時でIFぶんで分割する）
 # ③VIEWにレンダーを出す
 # https://qiita.com/nakanishi03/items/d1be27c74c98855423ab
+
+ # エラー複数のデータなのにEACHを使うと、下記のようなことが起こります
+#  Viewのなかに例えば2というIDを入力して
+  # バイプラ先生やったら問題なくできた
+
+  # 原則画面遷移する前に下記のアクションは発動してないので
+  # 画面遷移する前にインスタンス変数を生成する場合、インデックスであらかじめ生成しておく必要がある
